4aad5efcf8ad74b369ea299b420efd86773a9a16
Tabatha Slatton <valbatha@gmail.com>
2021-02-03 13:00:42 -0500

    Finished very simple crud application.

--- newtodo.php
+++ newtodo.php
@@ -9,7 +9,18 @@ $todoName = $_POST['todo_name'] ?? false;
 $todoName = trim($todoName);
 
 if($todoName){
-    $json = file_get_contents('todo.json');
-    echo $json;
-//    file_put_contents('todo.json');
+    if(file_exists('todo.json')) {
+        $json = file_get_contents('todo.json');
+        $jsonArray = json_decode($json, true);
+    } else {
+        $jsonArray = [];
+    }
+    $jsonArray[$todoName] = ['completed' => false];
+//    echo '<pre>';
+//        var_dump($jsonArray);
+//    echo '</pre>';
+    file_put_contents('todo.json', json_encode($jsonArray, JSON_PRETTY_PRINT));
 }
+
+//allows us to stay on this page
+header('location: index.php');
